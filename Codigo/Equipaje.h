49bcd2ca6acee49f478fcf248b22d9e22721cc65
JosueCarrion <carrionjosueandres@gmail.com>
2025-03-27 22:36:57 -0400

    corregir error al asignar prioridad

--- Codigo/Equipaje.h
+++ Codigo/Equipaje.h
@@ -4,10 +4,11 @@
 #include <string.h>
 #include "Cola.h"
 
-typedef struct {
-	int id;
-	char tipo[20];
-	char ciudad[50];
+typedef struct
+{
+    int id;
+    char tipo[20];
+    char ciudad[50];
     char pais[50];
     char vuelo[10];
     int idVuelo;
@@ -21,21 +22,33 @@ typedef struct {
 void construtorEquipaje(Equipaje *equipaje);
 int traducirPrioridad(char prioridad[]);
 
-void construtorEquipaje(Equipaje *equipaje){
+void construtorEquipaje(Equipaje *equipaje)
+{
     equipaje->id = 0;
     equipaje->peso = 0;
     equipaje->prioridad = 0;
 }
 
-int traducirPrioridad(char prioridad[]){
+int traducirPrioridad(char prioridad[])
+{
     int res = 0;
-    char Mano[] = "Mano",MANO[] = "MANO", mano[] = "mano";
-    char Facturado[] = "Facturado",FACTURADO[] = "FACTURADO", facturado[] = "facturado";
-    if(((strcmp(Mano,prioridad) == 0) || (strcmp(MANO,prioridad) == 0) || (strcmp(mano,prioridad) == 0))){
+    char Mano[] = "Mano", MANO[] = "MANO", mano[] = "mano";
+    char Sb[] = "Sobredimensionado", SB[] = "SOBREDIMENSIONADO", sb[] = "sobredimensionado";
+    char Facturado[] = "Facturado", FACTURADO[] = "FACTURADO", facturado[] = "facturado";
+    if (((strcmp(Mano, prioridad) == 0) || (strcmp(MANO, prioridad) == 0) || (strcmp(mano, prioridad) == 0)))
+    {
+        res = 4;
+    }
+    else if (((strcmp(Sb, prioridad) == 0) || (strcmp(SB, prioridad) == 0) || (strcmp(sb, prioridad) == 0)))
+    {
         res = 3;
-    }else if(((strcmp(Facturado,prioridad) == 0) || (strcmp(FACTURADO,prioridad) == 0) || (strcmp(facturado,prioridad) == 0))){
+    }
+    else if (((strcmp(Facturado, prioridad) == 0) || (strcmp(FACTURADO, prioridad) == 0) || (strcmp(facturado, prioridad) == 0)))
+    {
         res = 2;
-    }else{
+    }
+    else
+    {
         res = 1;
     }
     return (res);
